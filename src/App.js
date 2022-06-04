62c1b7c4150397db0d8afac05a3b2eb16b51755a
Lucas Joaquin Vanini <ljvanini92@gmail.com>
2022-06-04 12:29:34 -0300

    Entrega Contador

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 
 import './App.css';
+
 import ItemListContainer from './components/itemList/ItemListContainer';
 import MUNavBar from './components/MUNavBar';
+import ItemCount from './components/ItemCount';
+
+
 
 function App() {
+const onAdd = (count) => {
+  
+  alert(`Agregaste ${count} productos al carrito`)
+}
+
+
+
   return (
     <>
    <MUNavBar/>
    <ItemListContainer greetings={"Bienvenidos a la tienda oficial de Uni"}/>
+   <div className='Count'>
+   <ItemCount initial = {1} max = {10} onAdd = {onAdd}/>
+   </div>
    </>
   );
 }
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,57 @@
+
+import React, { useState } from 'react'
+
+
+
+const ItemCount = ({initial, max, onAdd}) =>{
+
+  
+
+  const [count, setCount] = useState(initial)
+
+
+
+const sumar = () =>{
+  if (count < max){
+    
+  
+  setCount(count + 1)
+}
+
+ else {
+  alert (`No puedes agregar mas productos a tu carrito`)
+}
+}
+
+const restar = () =>{
+
+
+  if (count > initial)
+  {
+  setCount(count - 1)
+
+  }
+  else{
+    alert (`No hay productos para sacar`)
+  }
+}
+
+
+
+ 
+    return (
+
+
+    <div>
+      <h2>{count}</h2>
+      <button onClick={sumar}>+</button>
+      <button onClick={restar}>-</button>
+      <button onClick={()=> onAdd(count)}>Agregar al Carrito</button>
+
+    </div>
+  )
+    }
+
+
+export default ItemCount
+
